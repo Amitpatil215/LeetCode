e3028e51b6bef2ba2199204d4213ccd991f03564
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-06-29 17:14:35 +0530

    Time: 27 ms (37.55%), Space: 9 MB (37.60%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -29,7 +29,33 @@ public:
     int minDistance(string word1, string word2) {
         int len1= word1.size();
         int len2= word2.size();
-        vector<vector<int>> dp(len1,vector<int>(len2,-1));
-        return md(len1-1, len2-1, word1, word2,dp);
+        
+        vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+        
+        
+        // base case
+        for(int index2=0;index2<=len2; index2++){
+            dp[0][index2] = index2;
+        }
+        
+        for(int index1=0;index1<=len1; index1++){
+            dp[index1][0] = index1;
+        }
+        
+        for(int index1=1; index1<=len1; index1++){
+            for(int index2=1; index2<=len2; index2++){
+                if(word1[index1-1]==word2[index2-1]){
+                    // found a match
+                    dp[index1][index2] = 0 + dp[index1-1][index2-1];
+                }else{
+                    int deleteOperation = dp[index1-1][index2];
+                    int insertOperation = dp[index1][index2-1];
+                    int replaceOperation = dp[index1-1][index2-1];
+                    dp[index1][index2] = 1 + min(deleteOperation, min(insertOperation,replaceOperation));
+                }
+            }
+        }
+        return dp[len1][len2];
+        
     }
 };
