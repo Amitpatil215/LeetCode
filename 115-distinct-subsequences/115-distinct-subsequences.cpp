eabddc2343ceae561f63a1030f35a322528709d6
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-06-29 13:39:43 +0530

    Time: 40 ms (76.30%), Space: 6.2 MB (99.22%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -8,25 +8,22 @@ public:
         int mod = (1e9 +7);
         
         vector<int> prev(l2+1, 0);
-        vector<int> curr(l2+1, 0);
         
         // for base case
         //we got our sbsequequence as we are follwoing 1 based indexing in this if index2==0
         prev[0] = 1;
-        curr[0] = 1; 
         
         for(int index1=1; index1<=l1; index1++){
-            for(int index2=1; index2<=l2; index2++){
+            for(int index2=l2; index2>=1; index2--){
                 if(str1[index1-1]==str2[index2-1]){
                     int take = prev[index2-1];
                     int doNotTake = prev[index2];
-                    curr[index2] = (take + doNotTake) % mod; 
+                    prev[index2] = (take + doNotTake) % mod; 
                 }else{
-                    curr[index2] = prev[index2];
+                    prev[index2] = prev[index2];
                 }
                 
             }
-            prev = curr;
         }
         return prev[l2];
     }
