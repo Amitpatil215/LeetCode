1c4c98588c8d9b92efedd0726ad85bf9ffed4b26
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-06-30 16:11:15 +0530

    Time: 11 ms (53.13%), Space: 13 MB (88.19%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -24,27 +24,31 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        vector<int> prev(2,0);
+        vector<int> curr(2,0);
+        
         for(int index=n;index>=0;index--){
             for(int isEarlierBought=0;isEarlierBought<2;isEarlierBought++){
                 
                 if(index==n){ //base case
-                    dp[index][isEarlierBought] = 0;
+                    prev[isEarlierBought] = 0;
                 }else{
                     if(isEarlierBought){
                         //we can only sell
-                        int willSell= prices[index] + dp[index+1][false];
-                        int willNotSell= 0 + dp[index+1][true];
-                        dp[index][isEarlierBought] = max(willSell, willNotSell);
+                        int willSell= prices[index] + prev[false];
+                        int willNotSell= 0 + prev[true];
+                        curr[isEarlierBought] = max(willSell, willNotSell);
                     }else{
                         // we can only buy
-                        int willBuy = -prices[index] + dp[index+1][true];
-                        int willNotBuy = 0 + dp[index+1][false];
-                        dp[index][isEarlierBought] = max(willBuy, willNotBuy);
+                        int willBuy = -prices[index] + prev[true];
+                        int willNotBuy = 0 + prev[false];
+                        curr[isEarlierBought] = max(willBuy, willNotBuy);
                     }    
                 }
-            }  
+            }
+            prev = curr;
         }
-        return dp[0][false];
+        return prev[false];
     }
 };
