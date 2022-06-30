ca3a1a0ec9ce0a479b955965e780d96f7b4b22c0
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-06-30 19:11:07 +0530

    Time: 8 ms (50.26%), Space: 11.2 MB (71.26%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,30 +1,37 @@
 
-// Tabulation
+// Tabulation + Space optimization
 class Solution {
    public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        vector<int> prev2(2, 0);
+        vector<int> prev1(2, 0);
+        vector<int> curr(2, 0);
+
         for (int index = n; index >= 0; index--) {
             for (int isEarlierBought = 0; isEarlierBought < 2;
                  isEarlierBought++) {
-                if (index >= n) {  // base case
-                    dp[index][isEarlierBought] = 0;
+                if (index == n) {  // base case
+                    prev1[isEarlierBought] = 0;
+                    prev2[isEarlierBought] = 0;
                 } else {
                     if (isEarlierBought) {
                         // we can only sell
-                        int willSell = prices[index] + dp[index + 2][false];
-                        int willNotSell = 0 + dp[index + 1][true];
-                        dp[index][isEarlierBought] = max(willSell, willNotSell);
+                        int willSell = prices[index] + prev2[false];
+                        int willNotSell = 0 + prev1[true];
+                        curr[isEarlierBought] = max(willSell, willNotSell);
                     } else {
                         // we can only buy
-                        int willBuy = -prices[index] + dp[index + 1][true];
-                        int willNotBuy = 0 + dp[index + 1][false];
-                        dp[index][isEarlierBought] = max(willBuy, willNotBuy);
+                        int willBuy = -prices[index] + prev1[true];
+                        int willNotBuy = 0 + prev1[false];
+                        curr[isEarlierBought] = max(willBuy, willNotBuy);
                     }
                 }
             }
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[0][false];
+        return prev1[false];
     }
 };
