bb1a62e34be5edd709986f8a77ce5bb0966594ee
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-06-30 20:30:03 +0530

    Time: 399 ms (12.49%), Space: 90.9 MB (29.76%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -23,7 +23,27 @@ public:
     }
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return mp(0,false,prices,fee,n,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        for(int index=n; index>=0; index--){
+            for(int isEarlierBought=0; isEarlierBought<2; isEarlierBought++){
+                if(index==n){
+                    dp[index][isEarlierBought] =0;
+                }else{
+                    if(isEarlierBought){
+                        // we can only sell
+                        int willSell = prices[index] - fee + dp[index+1][false]; 
+                        int willNotSell = 0 + dp[index+1][true];
+                        dp[index][isEarlierBought] = max(willSell,willNotSell);
+                    }else{
+                        // we can only buy
+                        int willBuy = - prices[index] + dp[index+1][true];
+                        int willNotBuy = 0 + dp[index+1][false];
+                        dp[index][isEarlierBought] = max(willBuy,willNotBuy);
+                    }
+                }
+            }
+        }
+        return dp[0][false];
     }
 };
