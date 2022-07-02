26cb80924d44cb1ada204e92635f72f42a6c282a
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-07-02 16:28:23 +0530

    Time: 13 ms (83.66%), Space: 10.2 MB (97.76%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,38 +1,18 @@
 class Solution {
-   public:
-    int lOLIS(int index, int earlierIndex, vector<int>& nums, int& n,
-              vector<vector<int>>& dp) {
-        if (index == n) {
-            return 0;
-        }
-        if (dp[index][earlierIndex + 1] != -1) {
-            return dp[index][earlierIndex + 1];
-        }
-
-        if (earlierIndex == -1 || nums[index] > nums[earlierIndex]) {
-            int take = 1 + lOLIS(index + 1, index, nums, n, dp);
-            int doNotTake = 0 + lOLIS(index + 1, earlierIndex, nums, n, dp);
-            return dp[index][earlierIndex + 1] = max(take, doNotTake);
-        } else {
-            int doNotTake = 0 + lOLIS(index + 1, earlierIndex, nums, n, dp);
-            return dp[index][earlierIndex + 1] = doNotTake;
-        }
-    }
-
+public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1);
+        vector<int> arr;
         
-        int maxLength=1;
-        for(int index=0; index<n;index++){
-            for(int earlierIndex =0; earlierIndex<=index-1;earlierIndex++){
-                if(nums[earlierIndex] < nums[index]){
-                    dp[index] = max(dp[index], 1 + dp[earlierIndex]);
-                }
+        arr.push_back(nums[0]);
+        for(int i=1; i<n; i++){
+            if(nums[i]>arr.back()){
+                arr.push_back(nums[i]);
+            }else{
+                int index = lower_bound(arr.begin(),arr.end(),nums[i]) - arr.begin();
+                arr[index] = nums[i];
             }
-            maxLength =max(maxLength, dp[index]);
         }
-        return maxLength;
-        
+        return arr.size();
     }
 };
