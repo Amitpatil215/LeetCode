d3376e06b3dbd1880baac9bd4f35d8dc8af70363
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-07-07 19:06:16 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,43 +5,36 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 public:
-    bool isBipartiteBFS(int startNode, vector<int> adj[], vector<int>&colours ){
-        queue<int> q;
+    bool isBipartiteDFS(int startIndex,vector<int>adj[], vector<int>&colors){
         
-        q.push(startNode);
-        colours[startNode]=0; //0 ->Blue 1-> Green
-        
-        while(!q.empty()){
-            int front = q.front();
-            q.pop();
-            
-            for(auto nbr: adj[front]){
-                if(colours[nbr]==-1){
-                    // node is not eralier visited i.e coloured
-                    // colour with opposite colour
-                    // if parents colour is 1 then assign 0 otherwise 1
-                    colours[nbr] = colours[front] == 1? 0 : 1;
-                    q.push(nbr);
-                }else{
-                    // if alredy coloured then
-                    // check if it has different colour than parent
-                    // otherwise return false as we cant form a bipertite graph
-                    if(colours[nbr]==colours[front]){
-                        return false;
-                    }
+        for(auto nbr: adj[startIndex]){
+            if(colors[nbr]==-1){
+                // node is not eralier visited i.e coloured
+                // colour with opposite colour
+                // if parents colour is 1 then assign 0 otherwise 1
+                colors[nbr] = colors[startIndex]==0 ? 1: 0;
+                bool isbp = isBipartiteDFS(nbr,adj,colors);
+                if(!isbp){
+                    return false;
+                }
+            }else{
+                // if alredy coloured then
+                // check if it has different colour than parent
+                // otherwise return false as we cant form a bipertite graph
+                if(colors[nbr] == colors[startIndex]){
+                    return false;
                 }
             }
         }
-        // if we able to colour component without any adjacent same colours 
-        // then we have a bipertite graph
         return true;
     }
 
 	bool isBipartite(int V, vector<int>adj[]){
-	    vector<int> colours(V,-1);
+	    vector<int> colors(V,-1);
         for(int i=0;i<V;i++){
-            if(colours[i]==-1){
-                if(!isBipartiteBFS(i,adj,colours)){
+            if(colors[i]==-1){
+                colors[i] = 0;
+                if(!isBipartiteDFS(i,adj,colors)){
                     return false;
                 }
             }
