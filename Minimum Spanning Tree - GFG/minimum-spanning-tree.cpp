b40c82558f94d9ca0ebda7201ac9a234f2da3fba
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-07-11 11:44:13 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,65 +4,98 @@ using namespace std;
 
  // } Driver Code Ends
 
+class disjoint_set{
+  private:
+  int n;
+  vector<int> Parent;
+  vector<int> Rank;
+  
+  void initilaize_parent(){
+    for(int i=0;i <n; i++){
+        Parent[i] = i;
+    }
+  }
+  
+  public:
+  disjoint_set(int n){
+    this->n = n;
+    Parent = vector<int>(n);
+    initilaize_parent();
+    Rank = vector<int>(n, 0);
+  }
+  
+  int find_parent(int node){
+    if(Parent[node] == node){
+        return node;
+    }
+    
+    // find parent recursively until the node is a parent of himself
+    // and also a Path compression step
+    return Parent[node] = find_parent(Parent[node]);
+  }
+  
+  void union_sets(int u, int v){
+    // find parent of u and v
+    u = find_parent(u); 
+    v = find_parent(v);
+    
+    // we attach to a node having higher rank
+    // if rank is same then increase rank of any one of the 
+    // parent node and attach to it
+    if(Rank[u]<Rank[v]){
+        Parent[u] =v;
+    }else if(Rank[u] > Rank[v]){
+        Parent[v] = u;
+    }else{
+        Rank[u]++;
+        Parent[v] = u;
+    }
+  }
+};
+
+bool comp(vector<int>&a,vector<int>&b){
+    return a[2] < b[2]; // sort according to weight
+}
 
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int v, vector<vector<int>> edges[])
+    int spanningTree(int v, vector<vector<int>> adj[])
     {
-        // build adjacecny list
-        vector<pair<int,int>> adjList[v];
-        
-        for(int i=0;i<v;i++){
-            for(auto edge:edges[i]){
-                adjList[i].push_back({edge[0],edge[1]});
-                adjList[edge[0]].push_back({i,edge[1]});
+        // build an edge list not adjacency lsit
+        vector<vector<int>> edgeList;
+        for(int i=0; i<v; i++){
+            for(auto x:adj[i]){
+                vector<int> edge(3);
+                edge[0] = i; // source
+                edge[1] = x[0]; // destination
+                edge[2] = x[1]; // weight
+                edgeList.push_back(edge);
             }
         }
         
-        // perform prims algo
-        vector<int> dist(v,INT_MAX);
-        vector<bool> mst(v,false);
-        vector<int> parent(v,-1);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minpq;
+        // cout<<edgeList.size()<<endl;
+        // sort according to weight to pick greedly
+        sort(edgeList.begin(),edgeList.end(),comp);
         
+        // pick edge with least weight
+        // if they belong to different set then exclude
+        // other wise consider in min spanning tree
+        int cost = 0 ;
         
-        dist[0] = 0;
-        minpq.push({0,0});
-        // as we gonna be having v-1 edges iterate for that no of times 
-        while(!minpq.empty()){
-            //choose minimum dist from dist array 
-            int minNode = minpq.top().second;
-
-            minpq.pop();
-            // consider min node as mst
-            mst[minNode] = true;
-            
-            // update the distance for its neighbours
-            for(auto nbr: adjList[minNode]){
-                int nbrNode = nbr.first;
-                int nbrWeight = nbr.second;
-                if( !mst[nbrNode] && nbrWeight < dist[nbrNode]){
-                    dist[nbrNode] =   nbrWeight;
-                    parent[nbrNode] = minNode;
-                    minpq.push({nbrWeight, nbrNode});
-                }
+        disjoint_set ds = disjoint_set(v);
+        
+        for(auto edge: edgeList){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            if(ds.find_parent(u)!=ds.find_parent(v)){
+                cost += wt;
+                ds.union_sets(u,v);
             }
-            
         }
-        // print edges
-        // for(int i=1;i< v; i++){
-        //     cout<<parent[i]<<"-"<<i <<"  "<<dist[i]<<endl;
-        // }
-        // cout<<endl;
-        
-        // find sum of weight
-        int sum = 0 ;
-        for(auto w:dist){
-            sum+=w;
-        }
-        return sum;
+        return cost;
     }
 };
 
