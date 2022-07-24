a5c53de2b5db8c981fca20dd7d21548fa31ab386
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-07-24 10:37:22 +0530

    Time: 181 ms (46.55%), Space: 14.7 MB (98.00%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -6,6 +6,10 @@ public:
         
         int row = 0;
         int col = m-1;
+        // start from the right top
+        // if top-right val is greater than target then we cant find value in that column so -> col--
+        // if top-right val is less than target then that means we cant find in the row so -> row++
+        // if top-right val is equal to target then return
         while(row <n && col>=0){
             if(matrix[row][col]>target){
                 col --;
