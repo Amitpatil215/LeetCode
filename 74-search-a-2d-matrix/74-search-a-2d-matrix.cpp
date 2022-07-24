a6e34ed477f63042a370938d9981f354fe76357e
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-07-24 09:55:24 +0530

    Time: 6 ms (60.03%), Space: 9.4 MB (78.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,15 +4,27 @@ public:
         int n =matrix.size();
         int m = matrix[0].size();
         
+        
+        /// reach to a row where the element lies
         int x=0,y=0;
         while(x<n-1 && matrix[x+1][y]<=target){
             x++;
         }
-        // cout<<x<<endl;
+        
+        /// linear search to find the element
+        /* 
         for(int i=0; i<m;i++){
             if(matrix[x][i]==target){
-                return true;
+            return true;
             }
+        }
+        */
+        
+        // instead we can apply lower bound i.e. we can leverage binary seach
+        
+        int index = lower_bound(matrix[x].begin(),matrix[x].end(),target) - matrix[x].begin();
+        if(index!=m && matrix[x][index]==target){
+            return true;
         }
         return false;
     }
