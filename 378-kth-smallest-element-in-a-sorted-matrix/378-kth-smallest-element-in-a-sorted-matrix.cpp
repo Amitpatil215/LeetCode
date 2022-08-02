9d246fadad61fb7d885a71a6bd318667bd8ddb6f
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-08-02 11:41:08 +0530

    Time: 95 ms (11.42%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int>maxpq;
-        int n=matrix.size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>minpq;
+        // add first element of all the rows i.e. first column
+        int n = matrix.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int num = matrix[i][j];
-                if(maxpq.size()<k){
-                    maxpq.push(num);    
-                }else{
-                    if(maxpq.top() > num){
-                        maxpq.pop();
-                        maxpq.push(num);
-                    }
-                }
-            }
+            minpq.push({matrix[i][0],{i,0}});
         }
-        return maxpq.top();
         
+        for(int i=0;i<k-1;i++){
+            auto top = minpq.top(); // ith smallest element
+            minpq.pop();
+            //push the next element from same row
+            int x =top.second.first;
+            int y =top.second.second+1;
+            if(y<n)
+            minpq.push({matrix[x][y],{x,y}});
+        }
+        return minpq.top().first;
     }
 };
