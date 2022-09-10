773b31f26f5038535ab66027d9b50329b6844993
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-09-10 22:30:44 +0530

    Time: 31 ms (28.29%), Space: 13.1 MB (35.44%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,39 +1,31 @@
-// Tabulation + space optimization
 class Solution {
-   public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-
-        vector<vector<int>> prev(2, vector<int>(k+1, 0));
-        vector<vector<int>> curr(2, vector<int>(k+1, 0));
-
-        for (int index = n; index >= 0; index--) {
-            for (int isEarlierBought = 0; isEarlierBought < 2;
-                 isEarlierBought++) {
-                for (int cap = 0; cap <= k; cap++) {
-                    if (index == n) {  // base case
-                        prev[isEarlierBought][cap] = 0;
-                    } else if (cap == 0) {
-                        curr[isEarlierBought][cap] = 0;
-                    } else {
-                        if (isEarlierBought) {
-                            // we can only sell
-                            int willSell = prices[index] + prev[false][cap - 1];
-                            int willNotSell = 0 + prev[true][cap];
-                            curr[isEarlierBought][cap] =
-                                max(willSell, willNotSell);
-                        } else {
-                            // we can only buy
-                            int willBuy = -prices[index] + prev[true][cap];
-                            int willNotBuy = 0 + prev[false][cap];
-                            curr[isEarlierBought][cap] =
-                                max(willBuy, willNotBuy);
-                        }
-                    }
-                }
-            }
-            prev = curr;
+public:
+    int mp(int index, bool isBuyingAllowed, int k, vector<int>& prices,vector<vector<vector<int>>> &dp){
+        
+        // base case
+        if(k==0){
+            return 0;
         }
-        return prev[false][k];
+        if(index == prices.size()){
+            return 0;
+        }
+
+        if(dp[index][isBuyingAllowed][k]!=-1){
+            return dp[index][isBuyingAllowed][k];
+        }
+        // recursive case
+        if(isBuyingAllowed){
+            int buy = - prices[index] + mp(index+1, false, k, prices,dp); 
+            int doNotbuy= mp(index+1, true, k, prices,dp);
+            return dp[index][isBuyingAllowed][k] = max(buy, doNotbuy); 
+        }else{
+            int sell = prices[index] + mp(index+1, true, k-1, prices,dp);
+            int doNotSell = mp(index+1, false, k, prices,dp);
+            return dp[index][isBuyingAllowed][k] = max(sell , doNotSell); 
+        }
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return mp(0, true, k, prices,dp);
     }
 };
