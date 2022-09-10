a7a00a0d66f47b423497679e582013e45b7b6528
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2022-09-10 23:37:29 +0530

    Time: 22 ms (45.96%), Space: 13.1 MB (27.76%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -24,8 +24,30 @@ public:
             return dp[index][isBuyingAllowed][k] = max(sell , doNotSell); 
         }
     }
-    int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return mp(0, true, k, prices,dp);
+    int maxProfit(int K, vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(K+1,0)));
+        // return mp(0, true, K, prices,dp);
+        for(int index=prices.size(); index>=0;index--){
+            for(int isBuyingAllowed = 0; isBuyingAllowed<=1; isBuyingAllowed++){
+                for(int k=0; k<=K; k++){
+                    if(index == prices.size()){
+                        dp[index][isBuyingAllowed][k] = 0;
+                    }else if(k==0){
+                        dp[index][isBuyingAllowed][k] = 0;
+                    }else{
+                        if(isBuyingAllowed){
+                            int buy = - prices[index] + dp[index+1][false][k];
+                            int doNotbuy=dp[index+1][true][k];
+                            dp[index][isBuyingAllowed][k] = max(buy, doNotbuy); 
+                        }else{
+                            int sell = prices[index] + dp[index+1][true][k-1];
+                            int doNotSell = dp[index+1][false][k];
+                            dp[index][isBuyingAllowed][k] = max(sell , doNotSell); 
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][true][K];
     }
 };
